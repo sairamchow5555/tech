7cd8916196f2fb5cacc6b1198963b99cd0761483
Sairam Chundru <sairam.chundru1432@gmail.com>
2023-11-21 18:03:04 +0530

    first commit

---
+++ src/components/home/about.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function AppAbout(){
+  return (
+    <div className="block">
+        <div className="container-fliud">
+            <div className="titleHolder">
+                <h2>About Us</h2>
+                <p></p>
+            </div>
+            <div className="contentHolder">
+                <p></p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AppAbout;
