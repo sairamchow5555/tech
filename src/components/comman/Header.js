55d7c7c0afbb8a6b17f2c3caad433810c7d91ed1
Sairam Chundru <sairam.chundru1432@gmail.com>
2023-11-24 11:46:43 +0530

    added anchor tags and functionalities

--- src/components/comman/Header.js
+++ src/components/comman/Header.js
@@ -1,7 +1,19 @@
-import React from 'react'
-import { Menu } from 'antd';
+import React, { useState } from 'react';
+
+import { Anchor, Drawer, Button } from 'antd';
+const { Link } = Anchor;
 
 function AppHeader(){
+    const [visible, setVisible] = useState(false);
+        
+    const showDrawer = () => {
+        setVisible(true);
+    };
+
+    const onClose = () => {
+        setVisible(false);
+    };
+
     return(
         <div className="container-fluid">
             <div className="header">
@@ -9,15 +21,39 @@ function AppHeader(){
                     <i className="fas fa-bolt"></i>
                     <a href="https://www.google.com">Tech</a>
                 </div>
-                <Menu mode="horizontal" defaultSelectedKeys={['home']}>
-                    <Menu.Item key="home">Home</Menu.Item>
-                    <Menu.Item key="about">About</Menu.Item>
-                    <Menu.Item key="features">Features</Menu.Item>
-                    <Menu.Item key="howitwork">How it work</Menu.Item>
-                    <Menu.Item key="faq">FAQ</Menu.Item>
-                    <Menu.Item key="pricing">Pricing</Menu.Item>
-                    <Menu.Item key="contact">Contact</Menu.Item>
-                </Menu>
+                <div className="mobileHidden">
+                    <Anchor targetOffset="65">
+                        <Link href="#hero" title="Home" />
+                        <Link href="#about" title="About" />
+                        <Link href="#feature" title="Feature" />
+                        <Link href="#works" title="How it works" />
+                        <Link href="#faq" title="FAQ" />
+                        <Link href="#pricing" title="Pricing" />
+                        <Link href="#contact" title="Contact" />
+                    </Anchor>
+                </div>
+                <div className="mobileVisible">
+                    <Button type="primary" onClick={showDrawer}>
+                        <i className="fas fa-bars"></i>
+                    </Button>
+                    <Drawer 
+                        placement="right"
+                        closable={false}
+                        onClose={onClose}
+                        visible={visible}
+                    >
+                        <Anchor targetOffset="65">
+                            <Link href="#hero" title="Home" />
+                            <Link href="#about" title="About" />
+                            <Link href="#feature" title="Feature" />
+                            <Link href="#works" title="How it works" />
+                            <Link href="#faq" title="FAQ" />
+                            <Link href="#pricing" title="Pricing" />
+                            <Link href="#contact" title="Contact" />
+                        </Anchor>
+                    </Drawer>
+                </div>
+                
             </div>
         </div>
     )
